16c6e437aa295b80ccee893e70365ad305942bd2
shubhra <shubhra.pandit@gmail.com>
2018-04-21 18:10:55 -0400

    Add some comments

--- src/PID.cpp
+++ src/PID.cpp
@@ -20,15 +20,18 @@ void PID::Init(double _Kp, double _Ki, double _Kd) {
 
 void PID::UpdateError(double cte) {
   
+  // the differential crosstrack error is given by cte(t) - cte(t-1)
   d_error = cte - p_error;
 
+  // the proportional error is the current crosstrack error
   p_error = cte;
   
+  // integrated crosstrack error is the sum of all previous crosstrack errors
   i_error += cte;
 }
 
 double PID::TotalError() {
   
-  return (-Kp * p_error - Kd * d_error - Ki * i_error);
+  return (- Kp * p_error - Kd * d_error - Ki * i_error);
 }
 
