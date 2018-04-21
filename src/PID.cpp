d9910681fa628838f218193838e343a775cf8b20
shubhra <shubhra.pandit@gmail.com>
2018-04-21 18:04:29 -0400

    Feature: Init, UpdateError and TotalError

--- src/PID.cpp
+++ src/PID.cpp
@@ -6,16 +6,29 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() : p_error(0), d_error(0), i_error(0) {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double _Kp, double _Ki, double _Kd) {
+  
+  Kp = _Kp;
+  Ki = _Ki;
+  Kd = _Kd;
+  
 }
 
 void PID::UpdateError(double cte) {
+  
+  d_error = cte - p_error;
+
+  p_error = cte;
+  
+  i_error += cte;
 }
 
 double PID::TotalError() {
+  
+  return (-Kp * p_error - Kd * d_error - Ki * i_error);
 }
 
